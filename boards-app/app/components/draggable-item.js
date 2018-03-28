230f29608f6b9b0863f6ea8793c77d8ce9a2c5ed
21602574 <jas@iut.fr>
2018-03-28 17:00:36 +0200

    fin de seance

---
+++ boards-app/app/components/draggable-item.js
@@ -0,0 +1,25 @@
+import Component from '@ember/component';
+import {computed} from '@ember/EmberObject';
+import {get} from '@ember/object';
+
+export default Component.extend({
+  classNameBindings: ['isActive'],
+  isActive: computed('content.active', function() {
+    let content=get(this,'content');
+    if(get(content,'active'))
+      return this.get('activeClass');
+    return '';
+  }),
+  attributeBindings : [ 'draggable' ],
+  draggable         : 'true',
+
+  dragStart(event) {
+    return event.dataTransfer.setData('text/data', get(this, 'content').get('id'));
+  },
+  doubleClick(){
+    this.sendAction("onDblClick",get(this,'content'));
+  },
+  click(){
+    this.sendAction("onClick",get(this,'content'));
+  }
+});
--- boards-app/app/models/story.js
+++ boards-app/app/models/story.js
@@ -5,5 +5,6 @@ export default DS.Model.extend({
   description:DS.attr('string'),
   developer: DS.belongsTo('developer'),
   project: DS.belongsTo('project',{inverse:'stories'}),
+  step: DS.belongsTo('step'),
   tags: DS.hasMany('tag')
 });
